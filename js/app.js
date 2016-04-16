67b71578c3790c92233f85ace6b6d1dba0103768
Arun <arunkumar@easasoft.com>
2016-04-17 01:21:44 +0530

    transfered to html

--- js/app.js
+++ js/app.js
@@ -4,39 +4,44 @@
     //map options
 
     app.controller('MapCtrl', function ($scope) {
-    var mapOptions = {
-    zoom: 12,
-    center: new google.maps.LatLng(13.073728, 80.225850),
-    mapTypeId: google.maps.MapTypeId.TERRAIN,
-        disableDefaultUI: true
-    }
+        
+
+        var mapOptions = {
+            zoom: 12,
+            center: new google.maps.LatLng(13.073728, 80.225850),
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            disableDefaultUI: true
+        }
+
+
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      $scope.coconut = 'img/';
+    $scope.coconut = 'img/';
     $scope.markers = [];
     $scope.kermarkers = [];
     
     var infoWindow = new google.maps.InfoWindow();
     
-      //creating function for marker
+      //creating function for markewr
     var createMarker = function (info){
     
     var marker = new google.maps.Marker({
-    map: $scope.map,
-    position: new google.maps.LatLng(info.lat, info.long),
-    title: info.location,
-          icon: $scope.coconut + 'coconut.png'
+        map: $scope.map,
+        position: new google.maps.LatLng(info.lat, info.long),
+        title: info.location,
+        icon: $scope.coconut + 'coconut.png',
+        content : '<div class="infoWindowContent">' + info.desc + '</div>'
+    });
+            
+    google.maps.event.addListener(marker, 'click', function(){
+
+        infoWindow.setContent('<h3>' + marker.title + '</h3>' + marker.content);
+        infoWindow.open($scope.map, marker);
+        $scope.map.setZoom(17);
+        $scope.map.setCenter(marker.getPosition());
     });
         
-    marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-    
-    google.maps.event.addListener(marker, 'click', function(){
-    infoWindow.setContent('<h3>' + marker.title + '</h3>' + marker.content);
-    infoWindow.open($scope.map, marker);
-          $scope.map.setZoom(17);
-    $scope.map.setCenter(marker.getPosition());
-        });
-        
     $scope.markers.push(marker);
+
     }
       // var kerMarker = function (info){
     
